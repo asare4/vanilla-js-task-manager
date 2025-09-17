790901002a19763814c784b9c5d1291167c0ab17
Asare <asare.ee4@gmail.com.github.com>
2025-09-17 19:59:49 +0000

    feat: update day logic and add help/tutorial function to task manager

---
+++ index.js
@@ -0,0 +1,27 @@
+let daySpan = document.querySelector(".day");
+let addBtn = document.querySelector(".addTasks");
+let count = document.querySelector(".allTaskCount");
+let list = document.querySelector(".allTasks ul");
+let help = document.querySelector(".help");
+
+function showHelp() {
+    alert(" Tutorial:\n\n1. Click '+ Add task' to create a new task.\n2. All your tasks will appear under 'All Tasks'.\n3. Use the Delete button to remove tasks.\n4. Tasks will be saved in your browser so that it can be accessed later.\n\nThat's it! ");
+}
+
+if (help) {
+    help.addEventListener("click", showHelp);
+}
+
+
+function showToday() {
+  let today = new Date();
+  let dayNumber = today.getDate();
+
+  if (dayNumber < 10) {
+    dayNumber = "0" + dayNumber;
+  }
+
+    daySpan.textContent = dayNumber;
+}
+
+document.addEventListener("DOMContentLoaded", showToday);
